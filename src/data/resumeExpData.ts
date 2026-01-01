d24a3cb8108e87ba2a77f325477c507f65a0fb17
irahulk01 <irahulkv@gmail.com>
2026-01-02 01:07:11 +0530

    refactor: restructure resume data and update experience display logic

---
+++ src/data/resumeExpData.ts
@@ -0,0 +1,84 @@
+// ---------- Certificates ----------
+import certificate1 from "../assets/Certificates/2023bootCamp.jpg";
+import certificate2 from "../assets/Certificates/2023_incl._React_Router_Redux.jpg";
+import certificate3 from "../assets/Certificates/FS_fynd.jpg";
+
+// ---------- Company Logos ----------
+import pisc from "../assets/CompanyLogo/pandoLogo.png";
+import nmsGames from "../assets/CompanyLogo/nmsGames.jpeg";
+import fyndImg from "../assets/CompanyLogo/fynd.png";
+import AiLogo from "../assets/CompanyLogo/aiLogo.png";
+import PixelConsultancyLogo from "../assets/CompanyLogo/pixelConsultancy.png";
+
+// ---------- Education ----------
+export const education = [
+  {
+    duration: "Aug 2016 – July 2020",
+    degree: "Bachelor's of Technology",
+    college: "Bengal College of Engineering and Technology",
+    university: "Maulana Abul Kalam Azaad University of Technology",
+  },
+  {
+    duration: "Nov 2021 – July 2022",
+    degree: "Higher Secondary School Certificate",
+    college: "Badri Narayan Saha DAV Public School",
+  },
+];
+
+// ---------- Experience ----------
+export const experience = [
+  {
+    company: "Pixel Consultancy",
+    role: "React Developer",
+    duration: "July 2025 – Present",
+    logo: PixelConsultancyLogo,
+    link: "https://www.pixelconsultancy.in/",
+  },
+  {
+    company: "Aggregate Intelligence",
+    role: "React Developer",
+    duration: "Jun 2024 – Jun 2025",
+    logo: AiLogo,
+    link: "https://aggregateintelligence.com/",
+  },
+  {
+    company: "Pando India Software Consultants",
+    role: "Frontend Developer",
+    duration: "Sep 2022 – Jun 2024",
+    logo: pisc,
+    link: "https://pandoconsultants.com/",
+  },
+  {
+    company: "NMS Games Private Ltd",
+    role: "Backend Developer",
+    duration: "Nov 2021 – Jul 2022",
+    logo: nmsGames,
+    link: "https://nmsgames.com/",
+  },
+  {
+    company: "Fynd Shopsense (Reliance backed)",
+    role: "Full Stack Developer Trainee",
+    duration: "Aug 2021 – Oct 2021",
+    logo: fyndImg,
+    link: "https://www.fynd.com/",
+  },
+];
+
+// ---------- Certifications ----------
+export const certifications = [
+  {
+    title: "Fynd FullStack MEVN 2021",
+    image: certificate3,
+    link: "https://drive.google.com/file/d/1rrRavvjyDoOUnj3c1RaEXGyfnLr3Dqr3/view",
+  },
+  {
+    title: "Web Development Bootcamp 2023",
+    image: certificate1,
+    link: "https://www.udemy.com/certificate/UC-f45447c1-ec65-4265-af55-a7951c7798ed/",
+  },
+  {
+    title: "React – The Complete Guide 2023 (React Router & Redux)",
+    image: certificate2,
+    link: "https://www.udemy.com/certificate/UC-573a7d6b-c10a-433b-b7b3-8141ed6c5e33/",
+  },
+];
---
+++ src/utills/experienceYear.js
@@ -0,0 +1,7 @@
+const years = Math.floor(diffInMonths / 12);
+const months = diffInMonths % 12;
+
+
+
+
+export default { years, months };
